3575e915f5caf5268262fede0789f05d2496d27f
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-03 19:22:20 -0300

    Added some ideas on how to test.

--- test.js
+++ test.js
@@ -1,16 +1,11 @@
 'use strict';
 
+/**
+* Ideas on how to test this:
+*
+* 1. Hook into `process.stdout.write`
+* 2. Shell out the test script to bash and inspect `stdout`: http://stackoverflow.com/a/637837/339827
+*/
+
 var write = require('./');
-var test = require('tape');
-
-var output = '';
-
-process.stdout.write = (function(callback) {
-    return function(string, encoding, fd) {
-        callback.apply(process.stdout, arguments)
-    }
-})(process.stdout.write);
-
-//write('foo' + Math.random() * 42);
-
-
+write('foo' + Math.random() * 42);
